8cc09b1500b9c42630562a0fbed2d3ee2be3ac00
Kshitiz <72270154+Kshitiz0302@users.noreply.github.com>
2024-06-27 15:14:50 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,28 @@
 import "./navbar.css";
-import React from 'react'
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <div className="navbar">
-        <div className="navContainer">
-            <span className="logo">
-              <img src="Logo.png" alt="" />
-            </span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-            </div>
+      <div className="navContainer">
+        <span className="logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
+          <img src="Logo.png" alt="Logo" />
+        </span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
